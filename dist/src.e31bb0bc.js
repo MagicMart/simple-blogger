428548f29c1751699f3d853b00333beabda0ff1a
Martin Tudor <martcan27@gmail.com>
2019-04-20 00:21:58 +0100

    update

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -43132,7 +43132,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42425" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43089" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
