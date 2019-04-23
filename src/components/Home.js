ff3f215681ea878ac32a14ad932fdc21cc4bbb40
Martin Tudor <martcan27@gmail.com>
2019-04-23 19:38:32 +0100

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -67,12 +67,12 @@ class Home extends React.Component {
                                         className="title"
                                         to={`/${obj.pathname}`}
                                     >
-                                        {obj.title}{" "}
                                         <small>
                                             <time className="date">
                                                 {obj.date}
                                             </time>
-                                        </small>
+                                        </small>{" "}
+                                        {obj.title}
                                     </Link>
                                 </li>
                             ))}
