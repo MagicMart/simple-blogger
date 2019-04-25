d06057b8cece23c297a7baee734165b40aedd423
Martin Tudor <martcan27@gmail.com>
2019-04-25 23:49:34 +0100

    tidy

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function Nav() {
     return (
         <nav>
-            <div>
-                <Link to="/">
-                    <h1 className="simple-blogger">Simple Blogger</h1>
-                </Link>
-            </div>
+            <Link to="/">
+                <h1 className="simple-blogger">Simple Blogger</h1>
+            </Link>
         </nav>
     );
 }
